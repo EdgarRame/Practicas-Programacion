5d40e637d89f36eb41b0887974f245de6587aed9
edgar <edgar_rame98@outlook.com>
2026-04-05 15:18:09 -0600

    Fundamentos de Python

---
+++ Python/fundamentos.py
@@ -0,0 +1,22 @@
+#Tipos de datos
+nombre = ("Edgar")
+edad = 27
+estudiante = True
+
+#Operaciones basicas
+numero_1 = 10
+numero_2 = 5
+resultado_suma = numero_1+numero_2
+resultado_resta = numero_1-numero_2
+resultado_multiplicacion= numero_1*numero_2
+resultado_division = numero_1/numero_2
+resultado_division_entera = numero_1//numero_2
+resultado_modulo = numero_1%numero_2
+
+# Salida al CMD
+print(f"Hola Mundo, mi nombre es {nombre} y tengo {edad} años")
+print(f"La división de {numero_1} entre {numero_2} es resultado{resultado_division:.2f}")
+
+#Entrada de datos
+usuario_input = input("¿Qué lenguaje estas aprendiendo? ")
+print(f"Genial que estes aprendiendo {usuario_input}, ojala lo domines.")
