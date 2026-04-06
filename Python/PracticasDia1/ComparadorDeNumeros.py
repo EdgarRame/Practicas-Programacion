a2a25cb2455ee636d94366af0f65c5fc198f8b2a
edgar <edgar_rame98@outlook.com>
2026-04-05 19:52:01 -0600

    Comparador de Numeros

---
+++ Python/PracticasDia1/ComparadorDeNumeros.py
@@ -0,0 +1,14 @@
+#Encabezado
+print("***Comparador de números***")
+print("Comparare si el primer número es mayor que el segundo número")
+#Entrada de datos
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+
+#Comparación de números
+if num1 > num2:
+    print(num1 > num2)
+elif num1 < num2:
+    print(num1 > num2)
+else:
+    print("Los números son iguales")
