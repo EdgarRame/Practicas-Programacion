fc590badedfa540dd4ffaa17784c20bfd261a2d3
edgar <edgar_rame98@outlook.com>
2026-04-05 19:29:30 -0600

    Cambiar Celcius a Fahrenheit

---
+++ Python/ConvetidorGrados.py
@@ -0,0 +1,7 @@
+# Inputs y Prints
+print("***Convertidor de Grados Celcius a fahrenheit***")
+
+grados = float(input("¿Cuántos grados Celcius son: "))
+fahrenheit = (grados * 9/5)+32
+
+print(f"Los grados Fahrenheit son: {fahrenheit:.1f}")
