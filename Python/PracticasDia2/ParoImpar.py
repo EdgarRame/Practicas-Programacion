08f2ff81ebe3f0576e77100bdf7f3b01baee3deb
edgar <edgar_rame98@outlook.com>
2026-04-06 10:18:08 -0600

    Verificar si un número es par o impar

---
+++ Python/PracticasDia2/ParoImpar.py
@@ -0,0 +1,12 @@
+# Cabezal
+print("**** Par o Impar ****")
+
+# Entrada de datos
+num = int(input("Ingrese un número: "))
+
+# Verificación
+
+if num % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
