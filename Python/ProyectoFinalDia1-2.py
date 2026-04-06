472b738ccbabdd6038f771889bc69678426af8ce
edgar <edgar_rame98@outlook.com>
2026-04-06 14:39:56 -0600

    ProyectoFinalDia1-2

--- Python/ProyectoFinalDia1-2.py
+++ Python/ProyectoFinalDia1-2.py
@@ -13,4 +13,19 @@ while True:
     precios = float(input(f"Ingrese el precio de {cosas}: "))
     gastos.append({"cosas": cosas, "precios": precios})
 
-print("\nReporte de gastos")
+if len(gastos) > 0:
+    total = sum(g['precios'] for g in gastos)
+    mas_caro = max(gastos, key = lambda g: g['precios'])
+    mas_barato = min(gastos, key = lambda g: g['precios'])
+    if total < 10000.00:
+        print("Los gastos son: ")
+        contador = 0
+        for i, elemento in enumerate (gastos, start=1):
+                print(f"{i}. {elemento['cosas']}")
+        print(f"Los costos totales son: {total:.2f}")
+        print(f"El gasto más caro fue {mas_caro['cosas']} con el precio de: (${mas_caro['precios']:.2f})")
+        print(f"El gasto más barato fue {mas_barato['cosas']} con el precio de: (${mas_barato['precios']:.2f})")
+    else:
+        print("Presupuesto excedido.")
+else:
+    print("No se añadireon gastos: ")
