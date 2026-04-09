96f8fedaa0957b9379a1f707554a4da834cf5e4d
edgar <edgar_rame98@outlook.com>
2026-04-09 13:52:19 -0600

    Ordenar las tarifas de mayor a menor

--- Python/Manipulacion_de_Datos/filtros.py
+++ Python/Manipulacion_de_Datos/filtros.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 # Opciones para mostrar el dataframe completo
-
 #pd.set_option('display.max_rows', None)
 
 # Llamado al csv
@@ -10,9 +9,14 @@ csv = pd.read_csv('titanic_limpio.csv')
 # Filtrando para mostrar unicamente los de primera clase y los que sobrevivieron
 filtrado = csv[(csv['Pclass'] == 1) & (csv['Survived'] == 1)]
 
+# Ordenado de mayor a menor en las tarifas de los boletos
+filtrado.sort_values(by = 'Fare', ascending = False, inplace = True)
+
 # Contar cuantos hombre sobrevivieron
 hombres = len(filtrado[(filtrado['Sex'] == 'male')])
 mujeres = len(filtrado[(filtrado['Sex']== 'female')])
+
+
 
 # Impreción del filtrado inicial
 print(filtrado)
