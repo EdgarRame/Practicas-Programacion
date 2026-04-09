5b64ebc27c06b0d20db29a4eb8b53d2494808c74
edgar <edgar_rame98@outlook.com>
2026-04-09 13:46:34 -0600

    Consultas especificas de un DataFrame

---
+++ Python/Manipulacion_de_Datos/filtros.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# Opciones para mostrar el dataframe completo
+
+#pd.set_option('display.max_rows', None)
+
+# Llamado al csv
+csv = pd.read_csv('titanic_limpio.csv')
+
+# Filtrando para mostrar unicamente los de primera clase y los que sobrevivieron
+filtrado = csv[(csv['Pclass'] == 1) & (csv['Survived'] == 1)]
+
+# Contar cuantos hombre sobrevivieron
+hombres = len(filtrado[(filtrado['Sex'] == 'male')])
+mujeres = len(filtrado[(filtrado['Sex']== 'female')])
+
+# Impreción del filtrado inicial
+print(filtrado)
+
+# Impresión de los hombre y mujeres que sobrevivieron
+print(f"Los hombres que sobrevivieron fueron: {hombres}")
+print(f"Las mujeres que sobrevivieron fueron: {mujeres}")
