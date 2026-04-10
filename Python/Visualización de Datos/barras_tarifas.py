38f85dcdeb45b1f31fc15012a6adb68d489e7c1d
edgar <edgar_rame98@outlook.com>
2026-04-10 13:47:58 -0600

    Manejo de visualización de datos con Grafica de Barras

---
+++ Python/Visualización de Datos/barras_tarifas.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Llamado al Dataset
+df = pd.read_csv('titanic_limpio.csv')
+
+# Configuración del estilo
+sns.set_theme(style = 'darkgrid')
+
+plt.figure(figsize = (8,5))
+sns.barplot(x = 'Pclass', y = 'Fare', data = df, palette = 'Oranges', hue = 'Pclass', legend = False,
+            errorbar = None)
+
+# Etiquetas de la grafica
+plt.title('Comparativa de Clases')
+plt.xlabel('Clases: (1 = Primera Clase 2 = Confort 3 = Economica)')
+plt.ylabel('Precios')
+
+# Se muestra la grafica
+plt.show()
