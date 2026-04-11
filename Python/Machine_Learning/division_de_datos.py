818898831732e5b24af49e1633e4e53a7b307629
edgar <edgar_rame98@outlook.com>
2026-04-11 13:54:50 -0600

    Actualización de códigos en el filtrado del Dataset

--- Python/Machine_Learning/division_de_datos.py
+++ Python/Machine_Learning/division_de_datos.py
@@ -3,7 +3,7 @@ import pandas as pd
 url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
 df = pd.read_csv(url)
 
-df['Embarked'] = df['Embarked'].map({'C': 0, 'S': 1, 'Q': 2}) # Se mapea el dataset convirtiento los valores a valores númericos
+df['Embarked'] = df['Embarked'].map({'C': 0, 'S': 1, 'Q': 2})
 df['Sex'] = df['Sex'].map({'female': 0, 'male': 1}) # Se mapea el dataset convirtiento los valores a valores númericos
 df = df[['Pclass', 'Sex', 'Age', 'Embarked','Survived']].dropna()
 
