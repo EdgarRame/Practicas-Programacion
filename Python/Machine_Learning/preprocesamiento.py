d3d636e59cd5a8f8f12d66de5bae281f12b7ae08
edgar <edgar_rame98@outlook.com>
2026-04-11 13:24:46 -0600

    Corrección de imports

--- Python/Machine_Learning/preprocesamiento.py
+++ Python/Machine_Learning/preprocesamiento.py
@@ -1,9 +1,4 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
 df = pd.read_csv(url)
